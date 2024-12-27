3f87b40c5da6372d9b216b6951a88cf2ddb65740
pingyujc <pingyujc@gmail.com>
2024-12-26 20:55:43 -0800

    init swift frontend and go backend

---
+++ frontend/iOS/DyeGo/DyeGo/DyeGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DyeGoApp.swift
+//  DyeGo
+//
+//  Created by Jonathan Chen on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DyeGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
