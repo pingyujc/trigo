a1441891db8819703c9d6c5c86713f5c4828e677
pingyujc <pingyujc@gmail.com>
2025-01-01 18:04:36 -0800

    can now build

---
+++ frontend/dyeGo/Others/dysGoApp.swift
@@ -0,0 +1,18 @@
+//
+//  dysGoApp.swift
+//  dyeGo
+//
+//  Created by Jonathan Chen on 1/1/25.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct dyeGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+} 
